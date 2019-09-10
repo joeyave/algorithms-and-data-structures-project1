505f2b1d7a9f2a717a74b3530d6a6d667f9e2df4
Joseph Aveltsev <aveltsev@gmail.com>
2019-09-10 17:31:14 +0300

    Add project files.

---
+++ lab1_db/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_db
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Birthday { get; set; }
+        public char Sex { get; set; }
+        public int Ssn { get; set; }
+        public bool IsFired { get; set; } = false;
+        
+        public Employee(string input)
+        {
+            string[] data = input.Split();
+
+            Name = data[0];
+            Surname = data[1];
+            Birthday = Convert.ToDateTime(data[2]);
+            Sex = Convert.ToChar(data[3]);
+            Ssn = Convert.ToInt32(data[4]);
+            if(data[5] != null && data[5] == "fired")
+                IsFired = true;
+        }
+
+        public Employee() { }
+
+        public override string ToString()
+        {
+            return $"[{Name} {Surname}, {Birthday.ToShortDateString()}, {Sex}, {Ssn}, {IsFired}]";
+        }
+    }
+}
---
+++ lab1_db/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_db
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines(@"C:\Users\josep\source\repos\lab1_db\lab1_db\DB.txt");
+
+            Employee[] employees = new Employee[lines.Length];
+
+            for(int i = 0; i < lines.Length; i++)
+            {
+                employees[i] = new Employee(lines[i]);
+            }
+
+            for(int i = 0; i < employees.Length; i++)
+            {
+                for(int j = i + 1; j < employees.Length; j++)
+                {
+                    if(employees[i].Ssn > employees[j].Ssn)
+                    {
+                        int temp = employees[i].Ssn;
+                        employees[i].Ssn = employees[j].Ssn;
+                        employees[j].Ssn = temp;
+                    }
+                }
+            }
+
+            foreach (Employee employee in employees)
+            {
+                if(employee.IsFired == false)
+                    Console.WriteLine(employee);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
